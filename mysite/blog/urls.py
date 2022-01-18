a1f315976a62a08dd1220de3bda2ad8aa1754bbb
aeSynk <danik_2198@mail.ru>
2022-01-18 14:24:17 +0600

    Added forms, HTML

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,6 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', post_list, name='post_list'),
-    path('post/<int:pk>/', post_detail, name='post_detail')
+    path('post/<int:pk>/', post_detail, name='post_detail'),
+    path('post/new/', post_new, name='post_new'),
+    path('post/<int:pk>/edit/', post_edit, name='post_edit'),
 
 ]
